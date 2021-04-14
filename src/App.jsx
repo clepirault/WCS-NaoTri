3b79404dfc06aefe0dad463d308405fe990bab33
Fabien Bataille <fabienbataille.dev@gmail.com>
2021-04-14 12:06:21 +0200

    Première proposition

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import SampleComponent from './components/SampleComponent';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-      <SampleComponent />
+      <Home />
     </div>
   );
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="homePage">
+      <div className="H"> </div>
+      <div className="bloc">
+        <h1 className="points de collecte">Points de collecte</h1>
+        <h1 className="enseignes">Enseignes</h1>
+        <h1 className="espace">Mon espace</h1>
+        <h1 className="defi">Défi !</h1>
+        <h1 className="a-propos">À propos</h1>
+      </div>
+      <div className="F"> </div>
+    </div>
+  );
+}
+
+export default Home;
