6b9b066fa76bc700a93db47e4eeb639d521ba4df
Clemence Pirault <clepirault@gmail.com>
2021-04-14 16:35:50 +0200

    added content icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import './App.css';
 import Footer from './components/Footer';
 import Social from './components/Social';
 import Home from './components/Home';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Home />
       <Social />
       <Footer />
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,30 @@
 import React from 'react';
 import './Home.css';
+import aboutIcon from '../images/aboutIcon.svg';
+import trophyIcon from '../images/trophyIcon.svg';
+import profileIcon from '../images/profileIcon.svg';
+import storeIcon from '../images/storeIcon.svg';
+import recycleBinIcon from '../images/recycleBinIcon.svg';
 
 function Home() {
   return (
     <div className="homePage">
-      <div className="bloc">
-        <h1 className="points de collecte">Points de collecte</h1>
-        <h1 className="enseignes">Enseignes</h1>
-        <h1 className="espace">Mon espace</h1>
-        <h1 className="defi">Défi !</h1>
-        <h1 className="a-propos">À propos</h1>
+      <div className="homeBloc">
+        <h1>
+          <img src={recycleBinIcon} alt="Recycle Bin" /> Points de collecte
+        </h1>
+        <h1>
+          <img src={storeIcon} alt="Stores" /> Enseignes
+        </h1>
+        <h1>
+          <img src={profileIcon} alt="Profil" /> Mon espace
+        </h1>
+        <h1>
+          <img src={trophyIcon} alt="Challenge" /> Défi !
+        </h1>
+        <h1>
+          <img src={aboutIcon} alt="About" />À propos
+        </h1>
       </div>
     </div>
   );
