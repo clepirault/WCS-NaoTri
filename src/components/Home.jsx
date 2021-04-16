0d0d051af13a17f7cd52f3c5de6825e2260c84dc
Fabien Bataille <fabienbataille.dev@gmail.com>
2021-04-16 12:19:37 +0200

    Prez 16/04

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,21 +10,26 @@ function Home() {
   return (
     <div className="homePage">
       <div className="homeBloc">
-        <h1>
-          <img src={recycleBinIcon} alt="Recycle Bin" /> Points de collecte
-        </h1>
-        <h1>
-          <img src={storeIcon} alt="Stores" /> Enseignes
-        </h1>
-        <h1>
-          <img src={profileIcon} alt="Profil" /> Mon espace
-        </h1>
-        <h1>
-          <img src={trophyIcon} alt="Challenge" /> Défi !
-        </h1>
-        <h1>
-          <img src={aboutIcon} alt="About" />À propos
-        </h1>
+        <div className="menu-collecte">
+          <img src={recycleBinIcon} alt="Recycle Bin" className="recycle-bin" />
+          <h1>Points de collecte</h1>
+        </div>
+        <div className="menu-enseignes">
+          <img src={storeIcon} alt="Stores" className="stores" />
+          <h1>Enseignes</h1>
+        </div>
+        <div className="menu-espace">
+          <img src={profileIcon} alt="Profil" className="profil" />
+          <h1>Mon espace</h1>
+        </div>
+        <div className="menu-defi">
+          <img src={trophyIcon} alt="Challenge" className="challange" />
+          <h1>Défi !</h1>
+        </div>
+        <div className="a-propos">
+          <img src={aboutIcon} alt="About" className="about" />
+          <h1>À propos</h1>
+        </div>
       </div>
     </div>
   );
