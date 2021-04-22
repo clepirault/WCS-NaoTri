d098f2e2b40b3b6aad8fd88e13420edbf2860196
Nicolas GERMAIN <n.germain.work@gmail.com>
2021-04-22 16:31:50 +0200

    localisation par CP

--- src/components/CollectMap.jsx
+++ src/components/CollectMap.jsx
@@ -36,7 +36,6 @@ const dataMaps = {
 };
 
 const CollectMap = () => {
-  // eslint-disable-next-line no-unused-vars
   const [center, setCenter] = useState({
     loaded: false,
     lat: profilUser.latitude,
@@ -44,9 +43,16 @@ const CollectMap = () => {
   });
   const ZOOM_LEVEL = 14;
 
+  // GEOLOCALISATION
+  const options = {
+    enableHighAccuracy: true,
+    timeout: 30000,
+    maximumAge: 27000,
+  };
+
   useEffect(() => {
     if ('geolocation' in navigator) {
-      navigator.geolocation.getCurrentPosition(
+      navigator.geolocation.watchPosition(
         (position) => {
           setCenter({
             loaded: true,
@@ -57,10 +63,56 @@ const CollectMap = () => {
         function b(error) {
           // eslint-disable-next-line no-console
           console.error(`error ${error.code} ${error.message}`);
-        }
+        },
+        options
       );
     }
   }, []);
+
+  // API code postal => coordonnées
+  const [msgHelp, setMsgHelp] = useState(
+    <div>
+      <p>
+        Si vous ne savez pas comment activer la géolocalisation, essayez ceci :`
+      </p>{' '}
+      <ul>
+        <li>
+          <a href="https://support.google.com/chrome/answer/142065?hl=en&co=GENIE.Platform%3DAndroid&oco=1">
+            Android (Chrome)
+          </a>
+        </li>
+        <li>
+          <a href="https://support.google.com/chrome/answer/142065?hl=en&co=GENIE.Platform%3DiOS&oco=1">
+            Apple (Chrome)
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+  const apiCPcoord = (cp) => {
+    axios
+      .get(`https://geo.api.gouv.fr/communes?codePostal=${cp}&format=geojson`)
+      .then((response) => {
+        if (!response.data.features.length) {
+          setMsgHelp('Code postal non valide, veuillez recharger la page.');
+        } else {
+          const coord = {
+            lat: response.data.features[0].geometry.coordinates[1],
+            lng: response.data.features[0].geometry.coordinates[0],
+          };
+          setCenter({
+            loaded: true,
+            lat: coord.lat,
+            lng: coord.lng,
+          });
+        }
+      });
+  };
+
+  const [cp, setCp] = useState('');
+  const handleCpSubmit = () => {
+    apiCPcoord(cp);
+  };
 
   // API
   let apiAerialColumn;
@@ -165,7 +217,29 @@ const CollectMap = () => {
 
   return (
     <div>
-      {center.loaded ? (
+      {!center.loaded ? (
+        <div>
+          <p>Pour afficher la carte, vous pouvez au choix :</p>
+          <ul>
+            <li>autoriser la géolocalisation</li>
+            <li>renseigner un code postal</li>
+          </ul>
+          <label htmlFor="cp">
+            Code Postal :
+            <input
+              type="text"
+              name="cp"
+              id="cp"
+              value={cp}
+              onChange={(e) => setCp(e.target.value)}
+            />
+          </label>
+          <button type="button" onClick={() => handleCpSubmit()}>
+            Valider
+          </button>
+          <p>{msgHelp}</p>{' '}
+        </div>
+      ) : (
         <MapContainer center={center} zoom={ZOOM_LEVEL}>
           <TileLayer
             url={dataMaps.tiles[0]}
@@ -290,8 +364,6 @@ const CollectMap = () => {
             ))}
           </MarkerClusterGroup>
         </MapContainer>
-      ) : (
-        ''
       )}
     </div>
   );
