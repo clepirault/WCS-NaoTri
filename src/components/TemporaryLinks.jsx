9187f432aa0151133b36623773aa1f8417fd9f6c
Nicolas GERMAIN <n.germain.work@gmail.com>
2021-04-23 10:04:01 +0200

    liens de navigation temporaires dans tips

---
+++ src/components/TemporaryLinks.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+
+import { Link } from 'react-router-dom';
+
+function TemporaryLinks() {
+  return (
+    <div>
+      <p />
+      <Link to="/slider">
+        <button type="button">slider</button>
+      </Link>
+      <p />
+      <Link to="/quiz0">
+        <button type="button">quiz0</button>
+      </Link>
+      <p />
+      <Link to="/quiz1">
+        <button type="button">quiz1</button>
+      </Link>
+      <p />
+      <Link to="/quiz2">
+        <button type="button">quiz2</button>
+      </Link>
+      <p />
+      <Link to="/quizResult">
+        <button type="button">quizResult</button>
+      </Link>
+      <p />
+      <Link to="/login">
+        <button type="button">login</button>
+      </Link>
+      <p />
+      <Link to="/home">
+        <button type="button">home</button>
+      </Link>
+      <p />
+      <Link to="/map">
+        <button type="button">map</button>
+      </Link>
+      <p />
+      <Link to="/shop">
+        <button type="button">shop</button>
+      </Link>
+      <p />
+      <Link to="/profile">
+        <button type="button">profile</button>
+      </Link>
+      <p />
+      <Link to="/tips">
+        <button type="button">tips</button>
+      </Link>
+      <p />
+      <Link to="/about">
+        <button type="button">about</button>
+      </Link>
+    </div>
+  );
+}
+
+export default TemporaryLinks;
