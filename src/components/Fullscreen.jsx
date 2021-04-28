591544832af8151060ca2eb1871f74db0c52afb0
Nicolas GERMAIN <n.germain.work@gmail.com>
2021-04-28 13:48:10 +0200

    version validée

--- src/components/Fullscreen.jsx
+++ src/components/Fullscreen.jsx
@@ -4,9 +4,8 @@ import { Link } from 'react-router-dom';
 import './Fullscreen.css';
 import downArrow from '../images/down.gif';
 
-const msg1 = `Pour profiter d'une expérience plein écran, slidez un peu vert le bas
-;)`;
-const msg2 = `Puis cliquez sur le bouton quand vous êtes tout en bas.`;
+const msg1 = `Pour profiter d'une expérience plein écran, slidez jusqu'à faire disparaître la barre d'adresse :)`;
+const msg2 = `Puis cliquez sur continuer.`;
 
 const Fullscreen = ({ setShowFooter }) => {
   useEffect(() => {
@@ -18,13 +17,13 @@ const Fullscreen = ({ setShowFooter }) => {
   return (
     <div className="Fullscreen">
       <div className="msgArrow">
+        <img src={downArrow} alt="downArrow" className="arrowDown" />
         <div className="msg">
           <p>{msg1}</p>
           <p>
             <em>{msg2}</em>
           </p>
         </div>
-        <img src={downArrow} alt="downArrow" className="arrowDown" />
       </div>
       <div className="bottom">
         <Link to="/home">
