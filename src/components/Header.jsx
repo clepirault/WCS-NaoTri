4a89600e3276fc31c462f8be04515b9d8e7ccd76
Nicolas GERMAIN <n.germain.work@gmail.com>
2021-04-30 12:10:17 +0200

    header permutable

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,12 +7,18 @@ import naotrilogo from '../images/naotrilogonob1.png';
 function Header(props) {
   // eslint-disable-next-line react/prop-types
   const { titre } = props;
+
   return (
     <div className="headerStyle">
       <div className="welcome">
-        <img className="img-header" src={naotrilogo} alt="" />
-        <h1 className="welcome-message">NAOTRI</h1>
-        <div className="headerStyle">{titre}</div>
+        {titre ? (
+          <div className="headerStyle">{titre}</div>
+        ) : (
+          <div>
+            <img className="img-header" src={naotrilogo} alt="" />
+            <h1 className="welcome-message">NAOTRI</h1>
+          </div>
+        )}
       </div>
     </div>
   );
