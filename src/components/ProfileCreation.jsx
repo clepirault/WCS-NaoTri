44094e002ce82c98d6121b5254b657817f4eeea1
NinonMaraval <79524843+NinonMaraval@users.noreply.github.com>
2021-04-30 09:58:12 +0200

    Revert "Profil creation local storage"

--- src/components/ProfileCreation.jsx
+++ src/components/ProfileCreation.jsx
@@ -1,84 +1,38 @@
-import React, { useState } from 'react';
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React from 'react';
 import './profileCreation.css';
-// link sera utilisé plus tard
 // eslint-disable-next-line no-unused-vars
 import { Link } from 'react-router-dom';
-import astronaut from '../images/astronaut.svg';
-import ninja from '../images/ninja.svg';
-import monster from '../images/monster.svg';
+import avatar from '../images/avatar.jpeg';
 
 function ProfileCreation() {
-  const [pseudo, setPseudo] = useState('');
-  const [birthday, setBirthday] = useState('');
-  const [email, setEmail] = useState('');
-
-  const handleClic = () => {
-    localStorage.setItem('pseudo', pseudo);
-    localStorage.setItem('birthday', birthday);
-    localStorage.setItem('email', email);
-  };
-  const handleChangePseudo = (event) => {
-    setPseudo(event.target.value);
-  };
-  const handleChangeBirthday = (event) => {
-    setBirthday(event.target.value);
-  };
-  const handleChangeEmail = (event) => {
-    setEmail(event.target.value);
-  };
-
   return (
     <div className="ProfileCreation">
       <h1 className="Naotri">NAOTRI</h1>
       <div className="picture">
+        <img src={avatar} alt="avatar" className="avatar" />
         <p>Choisis ton avatar</p>
-        <div>
-          <img src={ninja} alt="avatar" />
-          <img src={astronaut} alt="avatar" />
-          <img src={monster} alt="avatar" />
-        </div>
       </div>
       <div className="infos">
         <form className="pseudo">
-          <label htmlFor="pseudo">
-            Pseudo:
-            <input
-              type="text"
-              name="pseudo"
-              id="pseudo"
-              placeholder="Pierrafeu"
-              onChange={handleChangePseudo}
-              value={pseudo}
-            />
-          </label>
+          <label htmlFor="name">Entre ton Pseudo </label>
+          <input type="text" name="name" id="name" placeholder="Pierrafeu" />
         </form>
         <form className="birthday">
-          <label htmlFor="birthday">
-            Date de naissance:
-            <input
-              type="date"
-              name="birthday"
-              id="birthday"
-              onChange={handleChangeBirthday}
-              value={birthday}
-            />
-          </label>
+          <label htmlFor="date"> Ta date de naissance </label>
+          <input type="date" name="date" id="date" />
         </form>
         <form className="email">
-          <label htmlFor="email">
-            Email :
-            <input
-              type="email"
-              name="email"
-              id="email"
-              placeholder="fred.pierrafeu@naotri.com"
-              onChange={handleChangeEmail}
-              value={email}
-            />
-          </label>
+          <label htmlFor="email"> Ton couriel </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="fred.pierrafeu@naotri.com"
+          />
         </form>
         <form className="submit">
-          <button type="submit" id="submit" onClick={handleClic}>
+          <button type="submit" id="submit">
             Créer mon compte
           </button>
         </form>
