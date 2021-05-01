921549f73490755b0c7ae9972fe9c44010a20962
Nicolas GERMAIN <n.germain.work@gmail.com>
2021-05-02 00:45:28 +0200

    fixs pour header props/logo + local storage quiz

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,11 +10,11 @@ function Header(props) {
 
   return (
     <div className="headerStyle">
-      <div className="welcome">
+      <div>
         {titre ? (
           <div className="headerStyle">{titre}</div>
         ) : (
-          <div>
+          <div className="welcome">
             <img className="img-header" src={naotrilogo} alt="" />
             <h1 className="welcome-message">NAOTRI</h1>
           </div>
