7407a0275f1eb52a29be9da58817539f9e8deabb
Ninon Maraval <maravalninon@gmail.com>
2021-05-04 14:39:22 +0200

    css boutons

--- src/App.jsx
+++ src/App.jsx
@@ -85,7 +85,7 @@ function App() {
               <Fullscreen setShowFooter={setShowFooter} />
             </Route>
             <Route exact path="/slider">
-              <Slider />
+              <Slider setShowFooter={setShowFooter} />
             </Route>
             <Route exact path="/quiz0">
               <Quiz {...quizQuestions[0]} setShowFooter={setShowFooter} />
